f22f07da9cff628c301e0b433fd1f162ef36e309
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-09-06 09:07:54 +0300

    feat (cabin) : implementing the option menu

--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
   font-size: 1.4rem;
