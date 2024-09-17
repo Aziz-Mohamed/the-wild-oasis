2307dc67271a5b56746e6e3fd983c5b8641ed5f9
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-09-17 21:11:03 +0300

    Feat (bookings) : add filter and sort

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -6,11 +6,11 @@ import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
-  const {bookings, isLoading} = useBookings();
+  const { bookings, isLoading } = useBookings();
 
-  if(isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
 
-if(!bookings.length) return <Empty resourceName="bookings" />
+  if (!bookings.length) return <Empty resourceName="bookings" />;
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
