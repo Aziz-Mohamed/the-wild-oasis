b2ec890f524068ad7794056489786da2f96b2a87
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-09-19 19:08:23 +0300

    Feat (booking): add check-in confirmation.

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -7,7 +7,7 @@ import Menus from "../../ui/Menus";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
-import { HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 import { Navigate, useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
@@ -87,10 +87,22 @@ function BookingRow({
 
       <Menus.Menu>
         <Menus.Toggle id={bookingId} />
-        <Menus.List id={bookingId} >
-            <Menus.Button icon={<HiEye/> } onClick={()=> navigate(`/bookings/${bookingId}`)} >
+        <Menus.List id={bookingId}>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+          >
             See details
+          </Menus.Button>
+
+          {status === "unconfirmed" && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check in
             </Menus.Button>
+          )}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
